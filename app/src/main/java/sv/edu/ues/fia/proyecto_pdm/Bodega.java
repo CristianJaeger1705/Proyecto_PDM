d785d6874ad8be108ab4966160e39381e619501e
CristianJaeger1705 <146154761+CristianJaeger1705@users.noreply.github.com>
2026-05-11 23:16:02 -0600

    Agregue mis clases para empezar con el crud

---
+++ app/src/main/java/sv/edu/ues/fia/proyecto_pdm/Bodega.java
@@ -0,0 +1,42 @@
+package sv.edu.ues.fia.proyecto_pdm;
+
+public class Bodega {
+    private int idBodega;
+    private String nombreBodega;
+    private String departamento;
+    private String direccion;
+
+    public Bodega() {
+    }
+
+    public int getIdBodega() {
+        return idBodega;
+    }
+
+    public void setIdBodega(int idBodega) {
+        this.idBodega = idBodega;
+    }
+
+    public String getNombreBodega() {
+        return nombreBodega;
+    }
+
+    public void setNombreBodega(String nombreBodega) {
+        this.nombreBodega = nombreBodega;
+    }
+    public String getDepartamento(){
+            return departamento;
+    }
+
+    public void setDepartamento(String departamento){
+        this.departamento = departamento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
---
+++ app/src/main/java/sv/edu/ues/fia/proyecto_pdm/Seccion.java
@@ -0,0 +1,51 @@
+package sv.edu.ues.fia.proyecto_pdm;
+
+public class Seccion {
+    private int idSeccion;
+    private int idBodega;
+    private int nivel;
+
+    private int capacidadMax;
+
+    public Seccion() {
+    }
+
+    public Seccion(int idSeccion, int idBodega, int nivel, int capacidadMax) {
+        this.idSeccion = idSeccion;
+        this.idBodega = idBodega;
+        this.nivel = nivel;
+        this.capacidadMax = capacidadMax;
+    }
+
+    public int getIdSeccion() {
+        return idSeccion;
+    }
+
+    public void setIdSeccion(int idSeccion) {
+        this.idSeccion = idSeccion;
+    }
+
+    public int getIdBodega(){
+        return idBodega;
+    }
+
+    public void setIdBodega(int idBodega){
+        this.idBodega = idBodega;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getCapacidadMax() {
+        return capacidadMax;
+    }
+
+    public void setCapacidadMax(int capacidadMax) {
+        this.capacidadMax = capacidadMax;
+    }
+}
