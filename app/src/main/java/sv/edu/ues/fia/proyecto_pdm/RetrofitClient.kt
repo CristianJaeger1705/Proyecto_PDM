09d4863cac5de51c113e7789a2ecfb1cd982b4f2
CristianJaeger1705 <146154761+CristianJaeger1705@users.noreply.github.com>
2026-06-12 20:25:59 -0600

    corrigiendo errores

--- app/src/main/java/sv/edu/ues/fia/proyecto_pdm/RetrofitClient.kt
+++ app/src/main/java/sv/edu/ues/fia/proyecto_pdm/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:80/api/"
+    private const val BASE_URL = "http://192.168.100.172:80/api/"
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
